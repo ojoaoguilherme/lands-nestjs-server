93be2643d76ed410f262d62c4f7ff3a13881a965
ojoaoguilherme <joaoguilherme.pinheiro@outlook.com>
2023-09-28 14:59:43 -0300

    update typescript

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return 'ola Joao';
+    return 'ola daniel';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
-
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
